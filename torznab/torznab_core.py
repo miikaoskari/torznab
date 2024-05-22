130e09500a3f21a5e4f6cde1506b8f8da5648bd3
miika <miika.karjalainen13@gmail.com>
2024-05-22 22:53:08 +0300

    chore: Refactor Torznab class

--- torznab/torznab_core.py
+++ torznab/torznab_core.py
@@ -2,39 +2,42 @@
 import requests
 from lxml import objectify
 
-def caps(url, api_key=None):
-    # Get capabilities
-    payload = {}
-    headers = {}
-    response = None
+class Torznab:
+    def __init__(self, url, api_key=None):
+        self.url = url
+        self.api_key = api_key
 
-    if api_key is not None:
-        # Request with apikey
-        caps = f"?t=caps&apikey={api_key}"
-        response = requests.request("GET", url + caps, headers=headers, data=payload)
-    else:
-        # Request without apikey
-        caps = f"?t=caps"
-        response = requests.request("GET", url + caps, headers=headers, data=payload)
+    def caps(self):
+        return self._request("?t=caps")
+
+    def search(self):
+        return self._request("?t=search")
+
+    def tv_search(self):
+        return self._request("?t=tvsearch")
+
+    def movie(self):
+        return self._request("?t=movie")
+
+    def music(self):
+        return self._request("?t=music")
+
+    def book(self):
+        return self._request("?t=book")
+
+    def _request(self, params):
+        if self.api_key is not None:
+            params += f"&apikey={self.api_key}"
+        response = requests.get(self.url + params)
+        xml_content = response.content
+        print(xml_content)
+        return objectify.fromstring(xml_content)
     
-    xml_content = response.content
-    tree = objectify.fromstring(xml_content)
-
-    return tree
-
-def search():
-
-    pass
-
-def tv_search():
-    pass
-
-def movie():
-    pass
-
-def music():
-    pass
-
-def book():
-    pass
-
+if __name__ == "__main__":
+    import json
+    with open("key.json", "r") as f:
+        api = json.load(f)
+    torznab = Torznab(url=api["url"], api_key=api["key"])
+    result = torznab.caps()
+    print(result)
+    
