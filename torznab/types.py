f4f71e4f81d0bc7ba80e819db341bffdce061bc6
miikaoskari <miika.karjalainen13@gmail.com>
2025-09-14 03:52:12 +0300

    Add remaining attributes for torrentitem dataclass

--- torznab/types.py
+++ torznab/types.py
@@ -3,8 +3,12 @@ from dataclasses import dataclass
 @dataclass
 class TorrentItem:
     title: str | None = None
+    desc: str | None = None
     guid: str | None = None
+    comments: str | None = None
+    pub_date: str | None = None
+    size: str | None = None
     link: str | None = None
     size: str | None = None
-    categories: list[str] | None = None
-    attrs: dict[str, str] | None = None
+    categories: list[str | None] | None = None
+    attrs: dict[str, list[str]] | None = None
