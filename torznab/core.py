c89db4ab251b9d9f2b2d2f509f28fb2aa7e8f2e4
miikaoskari <miika.karjalainen13@gmail.com>
2025-09-22 15:07:04 +0300

    Add search_multiple_torrents for convenience

--- torznab/core.py
+++ torznab/core.py
@@ -2,16 +2,30 @@ import requests
 
 from .validators import is_url
 from .parser import parse_torznab
+from .types import TorrentItem
 
 class Torznab:
     def __init__(self, api_key: str) -> None:
         self.api_key = api_key
 
-    def search_torrent(self, query: str, url: str):
+    def _search(self, url: str, full_query: dict) -> str:
+        return requests.get(url, params=full_query).text
+
+    def search_torrent(self, query: str, url: str) -> list[TorrentItem] | None:
         try:
             is_url(url)
-            querystring = {"apikey":self.api_key,"t":"search","q":query}
-            response = requests.get(url, params=querystring)
-            return parse_torznab(response.text)
+            full_query = {
+                "t":"search",
+                "q":query,
+                **({"apikey": self.api_key} if self.api_key else {})
+            }
+            return parse_torznab(self._search(url, full_query))
+        except Exception as e:
+            print(e)
+
+    def search_multiple_torrents(self, query: str, urls: dict) -> list[TorrentItem] | None:
+        try:
+            for url in urls:
+                self.search_torrent(query, url)
         except Exception as e:
             print(e)
